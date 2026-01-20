92899f8fee04a83253afe594bac3ab13c05fab0c
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-20 10:37:12 +0530

    added starting content

--- my_version.py
+++ my_version.py
@@ -1,5 +1,6 @@
 """Random quiz generator which generates a quiz of india states capitals."""
 import random
+
 # Indian states(36 states, 28 states, 8 union territories)capitals dictionary.
 capitals = {
     "Andhra Pradesh": "Amaravati",
@@ -40,7 +41,13 @@ capitals = {
     "Ladakh": "Leh",
     "Jammu and Kashmir": "Srinagar",
 }
+
 # I am making 40 quizs so creating 40 quiz files
 for quiz_num in range(40):
     quiz_file = open(f'Quiz{quiz_num+1}.txt', 'w', encoding='utf-8')
     answer_file = open(f'Ans{quiz_num+1}.txt', 'w', encoding='utf-8')
+
+    # writing the starting content in my files
+    quiz_file.write("\n\nName:\n\nDate:\n\nRoll no.:\n\n")
+    quiz_file.write(" " * 20 + f"States Capitals Quiz (Set{quiz_num+1})")
+    quiz_file.write("\n\n")
