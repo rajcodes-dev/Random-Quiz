2d7904a4683f08650ac0f203b1c39f49cb749e95
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-20 10:36:27 +0530

    create 40quizfile

--- my_version.py
+++ my_version.py
@@ -1,6 +1,6 @@
 """Random quiz generator which generates a quiz of india states capitals."""
 import random
-
+# Indian states(36 states, 28 states, 8 union territories)capitals dictionary.
 capitals = {
     "Andhra Pradesh": "Amaravati",
     "Arunachal Pradesh": "Itanagar",
@@ -40,3 +40,7 @@ capitals = {
     "Ladakh": "Leh",
     "Jammu and Kashmir": "Srinagar",
 }
+# I am making 40 quizs so creating 40 quiz files
+for quiz_num in range(40):
+    quiz_file = open(f'Quiz{quiz_num+1}.txt', 'w', encoding='utf-8')
+    answer_file = open(f'Ans{quiz_num+1}.txt', 'w', encoding='utf-8')
